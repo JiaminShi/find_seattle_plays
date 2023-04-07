e1147d8e3e91edc6a0ae09e525f173918f4bbd40
JiaminShi <m.shi9610@gmail.com>
2023-04-07 01:28:47 -0700

    add tags

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-
 from urllib.request import urlopen
 import pprint
+
+end_page_offset = 6
+start_page_offset = 3
+tag_ids = [
+    '563',  # Seattle
+    '561',  # East Side
+    "560",  # North
+    "547"  # Tacoma & Olympia
+]
 
 def find_all_play_in_page(offset):
     url = 'https://nwtheatre.org/calendar/action~agenda/request_format~json/tag_ids~563/'
@@ -20,9 +28,15 @@ def find_all_play_in_page(offset):
         # find play name - hacky way, fix me
         index = play.find("@")
         if index > 0:
-            play_name = play[:index-1].strip()
+            play_name = play[:index - 1].strip()
             # find producer company - hacky way, fix me
             produce_index = play.find("</span>")
+            # find time
+            time_start_index = play.find('<div class="ai1ec-event-time">') + len('<div class="ai1ec-event-time">')
+            time_tmp = play[time_start_index:]
+            time_end_index = time_tmp.find('</div>')
+            time = time_tmp[:time_end_index].strip()
+            #
             if produce_index > 0:
                 produce = play[index:produce_index - 1].strip()
                 play_names[play_name] = produce
@@ -34,14 +48,9 @@ def find_all_play_in_page(offset):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    end_page_offset = 4
-    start_page_offset = 0
     all_plays = {}
     for offset in range(start_page_offset, end_page_offset):
         plays_in_current_offset = find_all_play_in_page(offset)
         all_plays = {**all_plays, **plays_in_current_offset}
     pprint.pprint(all_plays)
     print('finished')
-
-
-
